2c5c9a96c13322e8ee6228356fcf5e2495652715
Chuck Grimmett <cagrimmett@gmail.com>
2016-02-10 12:19:47 -0500

    Got the first version working!
    Cleared up some syntax errors, inserted the post_id to the database,
    made post_id a varchar, and changed author to author.name per
    http://stackoverflow.com/questions/20822808/praw-comment-submitters-user
    name/20939233#20939233

--- rAll.py
+++ rAll.py
@@ -19,32 +19,34 @@ top_posts = []
 def submission_fetcher(sub, limit):
     subreddit = r.get_subreddit(sub)
     for submission in subreddit.get_hot(limit=limit):
-        submission_data = {'title': submission.title, 'author': submission.author,
+        submission_data = {'title': submission.title, 'author': submission.author.name,
                            'num_comments': submission.num_comments, 'downs': submission.downs, 'ups': submission.ups,
-                           'score': submission.score, 'submission_id': submission.id}
+                           'score': submission.score, 'post_id': submission.id}
         top_posts.append(submission_data)
 
 def mysql_writer():
     with connection.cursor() as cursor:
-        sql = "CREATE TABLE IF NOT EXISTS `posts` (title VARCHAR(100) NOT NULL PRIMARY KEY, author VARCHAR(20) NOT NULL, num_comments SMALLINT NULL, downs INT NULL, ups INT NULL, score INT NULL, post_id INT NOT NULL)"
+        sql = "CREATE TABLE IF NOT EXISTS `posts` (title VARCHAR(100) NOT NULL PRIMARY KEY, author VARCHAR(20) NOT NULL, num_comments SMALLINT NULL, downs INT NULL, ups INT NULL, score INT NULL, post_id VARCHAR(30) NOT NULL)"
         
         cursor.execute(sql)
 
     for submission in top_posts:
         try:
             with connection.cursor() as cursor:
-                sql = "INSERT INTO `posts` (`title` `author`, `num_comments`, `downs`, `ups`, `score`, `submission_id`) VALUES (%s, %s, %s, %s, %s, %s, %s, )"
+                sql = "INSERT INTO `posts` (`title`, `author`, `num_comments`, `downs`, `ups`, `score`, `post_id`) VALUES (%s, %s, %s, %s, %s, %s, %s )"
                 cursor.execute(sql, (submission['title'],
                                      submission['author'],
                                      submission['num_comments'],
                                      submission['downs'],
                                      submission['ups'],
-                                     submission['score']
+                                     submission['score'],
+                                     submission['post_id']
                                      )
                                )
-                cursor.commit
+                connection.commit()
         finally:
-            connection.close()
+ #           connection.close()
+             pass
 
 submission_fetcher('trees', 10)
 mysql_writer()
